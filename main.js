ca3a78217bd34c1d532164078170f4fc62ee0fa6
AakilHussain07 <aakilhussain99@gmail.com>
2023-03-16 01:41:36 +0530

    firtassignmentjavascript

---
+++ main.js
@@ -0,0 +1,44 @@
+var a = "Aakil";
+console.log(a, typeof a);
+
+var b = 100;
+console.log(b, typeof b);
+
+var c = null;
+console.log(c, typeof c);
+
+var d = false;
+console.log(d, typeof d);
+
+var e
+console.log(e, typeof e);
+
+var f = Symbol();
+console.log(f, typeof f);
+
+
+var fruits=['mango','banana','pineapple','apple','orange'];
+console.log(fruits.length);
+console.log(fruits);
+
+fruits.push('cherry');
+console.log(fruits);
+
+fruits.unshift('banana');
+console.log(fruits);
+
+fruits.shift('mango');
+console.log(fruits);
+
+fruits.pop('apple');
+console.log(fruits);
+
+
+ var animal=['Tiger','Lion','Elephant','Leopard','Panther','Cheetah','Wolf','Jaguar','Hyena','Giraffe','Deer','Zebra','Gorilla'	,'Monkey','Chimpanzee','Bear','Wild Boar','Hippopotamus','Kangaroo'	,'Rhinoceros'];
+console.log(animal.length);
+console.log(animal);
+
+for(var i=0; i<animal.length; i++){
+    console.log(animal[i]);
+}
+
